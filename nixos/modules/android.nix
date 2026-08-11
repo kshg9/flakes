e48e32aa5a729725e063b2d911b07b5fd7a3542d
kshg9 <kshg9@users.noreply.github.com>
2026-08-12 02:17:12 +0530

    feat(android): explicitly declare buildTools and platform versions

--- nixos/modules/android.nix
+++ nixos/modules/android.nix
@@ -1,8 +1,16 @@
 { ... }: {
   flake.nixosModules.android = { pkgs, config, lib, ... }: let
-    predefine = pkgs.androidenv.androidPkgs;
-    buildToolsVersion = (builtins.head predefine.build-tools).version;
-    aapt2Path = "${predefine.androidsdk}/libexec/android-sdk/build-tools/${buildToolsVersion}/aapt2";
+    customSdk = pkgs.androidenv.composeAndroidPackages {
+      buildToolsVersions = [ "36.0.0" ];
+      platformVersions = [ "37" ];
+      includeSources = true;
+      includeEmulator = true;
+      includeSystemImages = true;
+      systemImageTypes = [ "google_apis_playstore" ];
+      abiVersions = [ "x86_64" ];
+    };
+    buildToolsVersion = (builtins.head customSdk.build-tools).version;
+    aapt2Path = "${customSdk.androidsdk}/libexec/android-sdk/build-tools/${buildToolsVersion}/aapt2";
   in {
     config = lib.mkIf config.extras.android.enable {
       users.groups.adbusers.members = [ "kdj" ];
@@ -14,13 +22,13 @@
 
       hjem.users.kdj = {
         packages = [
-          predefine.androidsdk
+          customSdk.androidsdk
           (pkgs.androidStudioPackages.canary.override { tiling_wm = true; })
           pkgs.jdk17
         ];
         environment.sessionVariables = {
-          ANDROID_HOME = "${predefine.androidsdk}/libexec/android-sdk";
-          ANDROID_SDK_ROOT = "${predefine.androidsdk}/libexec/android-sdk";
+          ANDROID_HOME = "${customSdk.androidsdk}/libexec/android-sdk";
+          ANDROID_SDK_ROOT = "${customSdk.androidsdk}/libexec/android-sdk";
           JAVA_HOME = "${pkgs.jdk17}";
           
           # Force Gradle to use the Nix-provided aapt2 (avoids download failures on read-only store)
